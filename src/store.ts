f16286af27e715d103a83c81bb595eb43c09e6d2
maechabin <mitoppo@gmail.com>
2018-08-10 01:43:00 +0900

    store作成

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+const SET_VALUE = 'SET_VALUE';
+const SET_INPUT_VALUE = 'SET_INPUT_VALUE';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    value: '',
+    inputValue: '',
   },
   mutations: {
-
+    [SET_VALUE](state, payload: { value: string }) {
+      state.value = payload.value;
+    },
+    [SET_INPUT_VALUE](state, payload: { value: string }) {
+      state.inputValue = payload.value;
+    },
   },
-  actions: {
-
-  },
+  actions: {},
 });
